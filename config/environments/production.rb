35b5395fb3adb6fcc511ab5c352d434f9ca70f7d
Erik <ewpeters@asu.edu>
2016-11-01 13:50:23 -0700

    Mailer fix again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -57,6 +57,9 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "gloria_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # for devise
+  config.action_mailer.default_url_options = { host: 'sendgrid.net' }
+  
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
