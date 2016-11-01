177be5a1f8082f949a9e8d295228c2e53670eb40
Erik <ewpeters@asu.edu>
2016-11-01 13:38:18 -0700

    Mailer fix

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,8 +2,9 @@ ActionMailer::Base.smtp_settings = {
    :address => "smtp.sendgrid.net",
    :port => 587,
    :domain => "sendgrid.net",
-   :user_name => "apikey",
-   :password => ENV["SENDGRID_API_KEY"],
+   :user_name => ENV["SENDGRID_USERNAME"],
+   :password => ENV["SENDGRID_PASSWORD"],
+   :domain => "sendgrid.net",
    :authentication => "plain",
    :enable_starttls_auto => true
  }
