c60abff316b4a9d82b7bcc8779318586f5fc6eca
Erik <ewpeters@asu.edu>
2016-12-21 19:43:01 -0700

    Title, apple icon, session timeout

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, :confirmable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :timeoutable, timeout_in: 16.hours
 
   after_save :skip_confirmation!
 
