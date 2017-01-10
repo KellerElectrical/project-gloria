179fd86934365faae88a160c2c152ccfa3a629d4
Erik <ewpeters@asu.edu>
2017-01-09 18:42:18 -0700

    remove fail

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
 
-  
+
   def check_signed_in
 		redirect_to new_session_url unless user_signed_in?
 	end
