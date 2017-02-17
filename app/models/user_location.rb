3c9996907de036ed773afe1e37f950cae630f2bf
Erik <ewpeters@asu.edu>
2017-02-17 11:56:02 -0700

    Try to_s again

--- app/models/user_location.rb
+++ app/models/user_location.rb
@@ -3,7 +3,7 @@ class UserLocation < ApplicationRecord
 
 	validates :user_id, :ip, presence: true	
 	# Tell geocoder which method returns a geocodable address
-	geocoded_by :ip
+	geocoded_by :ip_str
 	reverse_geocoded_by :latitude, :longitude, address: :address
 	after_validation :geocode, :reverse_geocode
 
@@ -12,6 +12,10 @@ class UserLocation < ApplicationRecord
 	def check_user_exists
 		user = User.find(self.user_id)
 		return false if user.nil?
-		self.ip = user.current_sign_in_ip.to_s
+		self.ip = user.current_sign_in_ip
+	end
+
+	def ip_str
+		self.ip.to_s
 	end
 end
