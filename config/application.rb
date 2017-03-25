b62707ce7970fa7756affe82dbdfde98aca2a208
Erik <ewpeters@asu.edu>
2017-03-24 18:07:11 -0700

    url pls

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,6 @@ module Gloria
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    Rails.application.routes.default_url_options[:host] = '???'
   end
 end
